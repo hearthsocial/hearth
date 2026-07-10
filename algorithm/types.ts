72189775788562130b5b8416dd931f38295cda8e
hhyyperion <hhyyperion@users.noreply.github.com>
2026-07-09 21:40:20 -0500

    added new type for getNewContent's parameters.

--- algorithm/types.ts
+++ algorithm/types.ts
@@ -1,6 +1,13 @@
 export type preferences = { tag: string; val: number | undefined }[];
-export type scoredTags = { tag: string; score: number }[]
+export type scoredTags = { tag: string; score: number }[];
 export interface preference {
   tag: string;
   val: number | undefined;
 }
+type feedType = "fyp" | "explore" | "following" | "friends";
+type contentType = "image" | "video" | "text";
+export type getContentParams =
+  | { feed: "mix"; type: "mix"; mixFeed: feedType[]; mixType: contentType[] }
+  | { feed: feedType; type: "mix"; mixFeed?: never; mixType: contentType[] }
+  | { feed: "mix"; type: contentType; mixFeed?: feedType[]; mixType?: never }
+  | { feed: feedType; type: contentType; mixFeed?: never; mixType?: never };
